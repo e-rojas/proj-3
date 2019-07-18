7a3bac4ab2970d3efae13dea1ec9975cb67bd894
javascript01fan <ejrojas@icloud.com>
2019-07-18 17:10:16 +0700

    axios working on the main frame bringing the places to the ui

--- src/RightCol.js
+++ src/RightCol.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
 import './Main.css'
 import logo from './images/logo.png';
-import food from './images/food.jpg';
+import Place from './Place'
+import axios from 'axios'
 export default class RightCol extends Component {
+  //data
+  state = {
+    places:[]
+  }
+  //Functions
+  componentWillMount(){
+    axios.get('http://localhost:2200/api/places').then((res)=>{
+      console.log(res)
+      this.setState({
+        places:res.data
+      })
+    }).catch((err)=>{
+      console.log('Error Axios Get ',err)
+    })
+  }
   render() {
     return (
 
@@ -13,30 +29,13 @@ export default class RightCol extends Component {
         </nav>
         <div id="dataDisplay" className="container-fluid mt-3">
           <div id="cardWrap" className=" p-3">
-
-            <div className="card mt-3 mb-3" >
-              <div className="card-body">
-                <div className="wrapper">
-                  <div className="row">
-                    <div className="col-3 p-0">
-                      <div className="imageWrapper "> <img src={food} alt="..." className="img-thumbnail"></img> </div>
-                    </div>
-                    <div className="col-9 d-flex flex-column"> <span><b>Thai-kitchen</b></span> <span><b><i className="far fa-address-book"></i> </b>Lamai Road 2345</span>
-                      <div>
-                        <span><b><i className="far fa-thumbs-up"></i> </b>13</span>
-                        <span><b><i className="far fa-thumbs-down"></i> </b>1</span>
-                      </div>
-                      <p className="m-0"><b><i className="far fa-sticky-note"></i> </b> Incredible place to eat!!</p>
-                      <p className="m-0"><b><i className="fas fa-at"></i> </b> Michael Bronk</p>
-                      <p className="m-0"><b><i className="far fa-calendar-alt"></i> </b> July 18 2019</p>
-                      <span className="badge badge-dark">#food</span>
-
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+           {/* PLACE */}
+           {
+              this.state.places.map((p)=>{
+                return  <Place place={p} key={p._id} />
+              })
+           }
+         
 
           </div>
 
